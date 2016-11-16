a8bd83e894b41059294d515cf09bd0c9208bc752
= <=>
2016-11-16 08:03:34 -0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="centered">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium deleniti dolores ipsum magni modi nesciunt
+            praesentium quod? Harum molestias, voluptates. Aliquam aspernatur deserunt doloribus
+            eaque est maxime, quos rerum voluptate.
+        </p>
+    </div>
+@endsection
